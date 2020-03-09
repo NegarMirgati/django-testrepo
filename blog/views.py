36dfea2707d6b7bd6c0cfd4f7cf278baa7d46fb5
Negar <negar.nbj96@yahoo.com>
2020-03-10 00:36:40 +0330

    add users app, add registeration form

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Negar Mirgati',
-        'title' : 'blog post 1',
-        'content' : 'bye bye bye',
-        'date' : 'August 28, 2019'
-    }
-    ,
-    {
-        'author' : 'Mehrdad Nourbakhsh',
-        'title' : 'blog post 2',
-        'content' : 'hi hi hi',
-        'date' : 'August 29, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context )
 
